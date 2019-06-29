2b4a7906c8637fdb1e7b645135d62393c6c1930a
Julie Kim <julie.kim@linecorp.com>
2019-06-29 16:52:20 +0900

    Init import

---
+++ src/main/java/org/julnamoo/immutable/ImmutableQueue.java
@@ -0,0 +1,26 @@
+package org.julnamoo.immutable;
+
+public class ImmutableQueue<T> implements Queue<T> {
+
+    @Override
+    public Queue<T> enQueue(T t) {
+        return null;
+    }
+    /**
+     * Removes the element at the beginning of the immutable queue, and returns the new queue.
+     */
+    @Override
+    public Queue<T> deQueue() {
+        return null;
+    }
+
+    @Override
+    public T head() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
