1e387be5c91b7d6579dd76fd05b4f439f7f8f12d
Julie Kim <julie.kim@linecorp.com>
2019-06-29 18:00:10 +0900

    Scratch the immutableQueue

--- src/main/java/org/julnamoo/immutable/ImmutableQueue.java
+++ src/main/java/org/julnamoo/immutable/ImmutableQueue.java
@@ -1,26 +1,70 @@
 package org.julnamoo.immutable;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 public class ImmutableQueue<T> implements Queue<T> {
+
+    private T[] items;
+    private int size;
+
+    public ImmutableQueue() {
+        this(4);
+    }
+
+    @SuppressWarnings("unchecked")
+    public ImmutableQueue(int initCapacity) {
+        if (initCapacity < 1) {
+            throw new IllegalArgumentException("initCapacity should be positive integer");
+        }
+        items = (T[]) new Object[initCapacity];
+        size = 0;
+    }
+
+    private ImmutableQueue(T[] items) {
+        this.items = items;
+        size = items.length;
+    }
 
     @Override
     public Queue<T> enQueue(T t) {
-        return null;
+        if (Objects.isNull(t)) {
+            throw new NullPointerException();
+        }
+
+        if (size + 1 < Integer.MAX_VALUE) {
+            items = Arrays.copyOf(items, size + 1);
+            items[size++] = t;
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        return this;
     }
+
     /**
      * Removes the element at the beginning of the immutable queue, and returns the new queue.
      */
     @Override
+    @SuppressWarnings("unchecked")
     public Queue<T> deQueue() {
-        return null;
+        if (items.length == 0) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        T[] newItems = (T[]) new Object[size - 1];
+        System.arraycopy(items, 1, newItems, 0, size - 1);
+        return new ImmutableQueue<>(newItems);
     }
 
     @Override
     public T head() {
-        return null;
+        if (items != null && items.length > 0) {
+            return items[0];
+        }
+        throw new ArrayIndexOutOfBoundsException();
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return items == null || size == 0;
     }
 }
---
+++ src/test/java/org/julnamoo/immutable/ImmutableQueueTest.java
@@ -0,0 +1,62 @@
+package org.julnamoo.immutable;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.stream.IntStream;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ImmutableQueueTest {
+
+    private ImmutableQueue<Integer> queue;
+
+    @Before
+    public void setUp() throws Exception {
+        queue = new ImmutableQueue<>(1);
+    }
+
+    @Test
+    public void enQueue() throws Exception {
+        IntStream.range(0, 1000000)
+                 .forEach(i -> queue.enQueue(i));
+    }
+
+    @Test
+    public void deQueue() throws Exception {
+        // enqueue first
+        IntStream.range(0, 1000000)
+                 .forEach(i -> queue.enQueue(i));
+
+        // try dequeue as much as items
+        IntStream.range(0, 1000000)
+                 .forEach(i -> queue.deQueue());
+    }
+
+    @Test
+    public void head() throws Exception {
+        // enqueue first
+        IntStream.range(0, 1000000)
+                 .forEach(i -> queue.enQueue(i));
+
+        // try dequeue as much as items
+        long dequeued = IntStream.range(0, 1000000)
+                                 .mapToObj(i -> queue.head())
+                                 .count();
+        assertEquals(1000000, dequeued);
+    }
+
+    @Test
+    public void isEmpty() throws Exception {
+        // didn't enqueue
+        assertTrue(queue.isEmpty());
+
+        queue.enQueue(Integer.MAX_VALUE);
+        assertFalse(queue.isEmpty());
+
+        queue.deQueue();
+        assertTrue(queue.isEmpty());
+    }
+}
